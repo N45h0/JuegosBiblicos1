42695402ae33f062018ae436f8429c9f3d8a9573
N45h0 <70018907+N45h0@users.noreply.github.com>
2025-01-31 01:24:51 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import BibleGame from './components/BibleGame'
-import './styles/main.css'
+import './public/static/css/main.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
